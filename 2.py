e8eeb5e7226f9bf2f6f645b1b1e7bd6e86eda27f
Farshad Asadpour <farshad_asadpour@tapin.ir>
2022-01-29 01:17:02 +0330

    Q2 pass

--- 2.py
+++ 2.py
@@ -7,10 +7,10 @@ seed(2)
 # fill the empty list to rand number
 for i in range(10):
     numbers_list.append(randint(0,1000))
+print("befor {}".format(numbers_list))
 
-print(numbers_list)
 for i in range(0,len(numbers_list),2):
     swap = numbers_list[i]
     numbers_list[i] = numbers_list[i+1]
     numbers_list[i+1] = swap
-print(numbers_list)   
+print("after {}".format(numbers_list))
