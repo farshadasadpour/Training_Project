0f47e8867cd05c06791844a5e4eba7b4232082c4
Farshad <asadpoor.f@gmail.com>
2022-01-29 09:36:51 +0330

    correction

--- 2.py
+++ 2.py
@@ -4,13 +4,15 @@ from random import seed,randint
 # init the empty list
 numbers_list=[]
 seed(2)
+
 # fill the empty list to rand number
-for i in range(10):
-    numbers_list.append(randint(0,1000))
+
+# for i in range(10):
+#     numbers_list.append(randint(0,1000))
+
+numbers_list = [randint(0,1000) for i in range(10)]
 print("befor {}".format(numbers_list))
 
 for i in range(0,len(numbers_list),2):
-    swap = numbers_list[i]
-    numbers_list[i] = numbers_list[i+1]
-    numbers_list[i+1] = swap
+    numbers_list[i],numbers_list[i+1] = numbers_list[i+1],numbers_list[i]
 print("after {}".format(numbers_list))
