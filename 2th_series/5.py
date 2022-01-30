8040f8a509185ed9f965b8f5e556be6fcfb601c1
Farshad <asadpoor.f@gmail.com>
2022-01-30 09:22:26 +0330

    change name file

--- 2th_series/5.py
+++ 2th_series/5.py
@@ -1,8 +1,8 @@
-from ast import Pass
-from operator import contains
 import string
 import random
+
 D = S = 0
+
 counts = int(input("lenth:"))
 char_list_1 = random.choices(string.ascii_uppercase, k = counts)
 char_list_2 = random.choices(string.ascii_uppercase, k = counts)
@@ -12,5 +12,6 @@ for i,j in zip(char_list_1,char_list_2):
         S+=1
     else:
         D+=1
+        
 print(f"diffrent is {D} and the same is {S}")
     
