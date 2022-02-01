bec5e13cd1cea1b3fbd69b2b61055d740902672f
Farshad <asadpoor.f@gmail.com>
2022-02-01 11:00:57 +0330

    change Q6

--- 2th_series/6.py
+++ 2th_series/6.py
@@ -4,6 +4,6 @@ dict_number = dict()
 
 seed(None)
 for i in range(100):
-    dict_number={**dict_number, str(randint(1,1000)):randint(0,10000000000)}
+    dict_number={**dict_number, str(randint(1,1000)):randint(0,1000)}
 
 max_value_key = max(dict_number.items(),key=operator.itemgetter(1))[0]
