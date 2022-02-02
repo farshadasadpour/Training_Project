72304e3a936e8c4af48cc0919a9b1bc53a3f3727
Farshad <asadpoor.f@gmail.com>
2022-02-02 16:03:23 +0330

    bug fix Q5

--- 2th_series/5.py
+++ 2th_series/5.py
@@ -1,21 +1,20 @@
 import string
 import random
 
-def list_gerenrator():
-    D = S = 0
+def list_gerenrator(counts):
+    diff = same = 0
 
-    counts = int(input("lenth:"))
     char_list_1 = random.choices(string.ascii_uppercase, k = counts)
     char_list_2 = random.choices(string.ascii_uppercase, k = counts)
 
     for i,j in zip(char_list_1,char_list_2):
         if i==j:
-            S+=1
+            same+=1
         else:
-            D+=1
+            diff+=1
             
     print(char_list_1,"\n",char_list_2)
-    print(f"diffrent is {D} and the same is {S}")
+    print(f"diffrent is {diff} and the same is {same}")
 
-
-list_gerenrator()
+counts = int(input("lenth:"))
+list_gerenrator(counts)
