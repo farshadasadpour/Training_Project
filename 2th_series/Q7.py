568f950ede4a65a6a352fca7ab9ace952d73d41e
Farshad <asadpoor.f@gmail.com>
2022-02-02 16:22:16 +0330

    Q11 pass

--- 2th_series/Q7.py
+++ 2th_series/Q7.py
@@ -1,9 +1,12 @@
 def def_1():
+    num_list = [1,2,3,4,5]
     new_list = []
-    for i in range(0,6):
+    for i in num_list:
         sum_nums = 0
-        for j in range(0,i+1):
+        for j in range(0,i):
             sum_nums+=j
         new_list.append(sum_nums+100)
 
     return(new_list)
+
+print(def_1())
